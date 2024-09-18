c62e6570d16d814fbd1e9a85794160c36a99772a
Arvind Kathare <arvind.kathare@thevouch.digital>
2024-09-18 18:06:56 +0530

    firast commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+
+import { Routes, Route } from 'react-router-dom'
+import "./globals.css";
+import SigninForm from './_auth/forms/SigninForm';
+import AuthLayout from './_auth/AuthLayout';
+import SignupForm from './_auth/forms/SignupForm';
+import RootLayout from './_root/RootLayout';
+import { Toaster } from "@/components/ui/toaster"
+import { AllUsers, CreatePost, EditPost, Explore, Home, LikedPosts, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
+
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore/>} />
+          <Route path="/saved" element={<Saved/>} />
+          <Route path="/liked" element={<LikedPosts/>} />
+          <Route path="/all-users" element={<AllUsers/>} />
+          <Route path="/create-post" element={<CreatePost/>} />
+          <Route path="/update-post/:id" element={<EditPost/>} />
+          <Route path="/posts/:id" element={<PostDetails/>} />
+          <Route path="/profile/:id/*" element={<Profile/>} />
+          <Route path="/update-profile/:id" element={<UpdateProfile/>} />
+        </Route>
+      </Routes>
+
+      <Toaster />
+    </main>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './context/AuthContext';
+import { QueryProvider } from './lib/react-query/QueryProvider.tsx';
+import React from 'react';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+     <BrowserRouter>
+    <QueryProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryProvider>
+  </BrowserRouter>
+  </React.StrictMode>
+ 
+
+)
